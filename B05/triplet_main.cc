a8894344a98f389244b87a9ba8975b0ebaddcdef
mme <oz322@stud.uni-heidelberg.de>
2024-01-08 21:27:19 +0300

    minor changes

--- B05/triplet_main.cc
+++ B05/triplet_main.cc
@@ -6,7 +6,8 @@ triplet_main.cc, indem Sie alle Methoden einmal aufrufen und Ausgaben dafür erz
 #include <string>
 #include "triplet.hh"
 
-void show(const Triplet& triplet) {
+template <typename T1, typename T2, typename T3>
+void show(const Triplet<T1, T2, T3>& triplet) {
     std::cout << "first : " << triplet.first() << std::endl;
     std::cout << "second : " << triplet.second() << std::endl;
     std::cout << "third : " << triplet.third() << std::endl;
@@ -16,10 +17,11 @@ int main() {
     Triplet<int, double, char> triplet(42, 3.14, 'F');
     std::cout << "Initial triplet: " << std::endl;
     show(triplet);
+    std::cout << std::endl;
 
     triplet.setFirst(57);
     triplet.setSecond(2.78);
-    triplet.setThird('ß');
+    triplet.setThird('R');
     std::cout << "Modified triplet: " << std::endl;
     show(triplet);
 }
