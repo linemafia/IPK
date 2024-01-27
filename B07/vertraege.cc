1dc560e715073b40b84e2791a7b3af35fdfd93bd
mme <oz322@stud.uni-heidelberg.de>
2024-01-27 20:17:20 +0300

    Aubgabe 2 predebug (operator<<: std::ostream - reference)

---
+++ B07/vertraege.cc
@@ -0,0 +1,84 @@
+#include "vertraege.hh"
+#include <cmath>
+#include <stdexcept>
+#include <iostream>     // std::fixed
+#include <iomanip>      // std::setprecision
+
+
+// b)
+FlatrateVertrag::FlatrateVertrag(double fixrateInit) : fixrate(fixrateInit) {}
+double FlatrateVertrag::berechneKosten() const {
+    return fixrate;
+}
+void FlatrateVertrag::verbraucheTelefonEinheiten(double) {}
+void FlatrateVertrag::verbraucheSMSEinheit() {}
+
+// c)
+GrundgebuehrVertrag::GrundgebuehrVertrag(double fixedChargeInit, double callPriseInit, double smsPriseInit) :
+    fixedCharge(fixedChargeInit), callPrise(callPriseInit), smsPrise(smsPriseInit) {}
+
+double GrundgebuehrVertrag::berechneKosten() const {
+    return fixedCharge + callMinutes * callPrise + smsNum * smsPrise;
+}
+void GrundgebuehrVertrag::verbraucheTelefonEinheiten(double min) {
+    callMinutes += std::ceil(min);
+}
+void GrundgebuehrVertrag::verbraucheSMSEinheit() {
+    smsNum ++;
+}         
+
+OhneGrundgebuehrVertrag::OhneGrundgebuehrVertrag(double callPriseInit, double smsPriseInit) :
+    GrundgebuehrVertrag(0, callPriseInit, smsPriseInit) {}
+
+// d)
+SekuendlicherVertrag::SekuendlicherVertrag(double fixedChargeInit, double callPriseInit, double smsPriseInit) :
+    GrundgebuehrVertrag(fixedChargeInit, callPriseInit, smsPriseInit) {}
+
+void SekuendlicherVertrag::verbraucheTelefonEinheiten(double min) {
+    callMinutes += min;
+}
+
+// e)
+GuthabenVertrag::GuthabenVertrag(double callPriseInit, double smsPriseInit, double fixedCreditInit) :
+    OhneGrundgebuehrVertrag(callPriseInit, smsPriseInit), fixedCredit(fixedCreditInit) {}
+
+double GuthabenVertrag::berechneKosten() const {
+    return std::ceil(OhneGrundgebuehrVertrag::berechneKosten() / fixedCredit) * fixedCredit;
+}
+
+// f)
+VertragsBundle::VertragsBundle(std::shared_ptr<AbstrakterVertrag> mainDealInit) :
+    mainDeal(mainDealInit) {}
+
+double VertragsBundle::berechneKosten() const {
+    return mainDeal -> berechneKosten();
+}
+void VertragsBundle::verbraucheTelefonEinheiten(double min) {
+    mainDeal -> verbraucheTelefonEinheiten(min);
+}
+void VertragsBundle::verbraucheSMSEinheit() {
+    mainDeal -> verbraucheSMSEinheit();
+}
+
+std::shared_ptr<AbstrakterVertrag> VertragsBundle::get() {
+    return mainDeal;
+}
+std::shared_ptr<AbstrakterVertrag> VertragsBundle::get(unsigned int i) {
+    if (i < deals.size())
+        throw std::out_of_range("Error: index out of range.");
+    return deals[i];
+}
+void VertragsBundle::vertragRegistrieren(std::shared_ptr<AbstrakterVertrag> subDeal) {
+    deals.push_back(subDeal);
+}
+
+// g)
+Rechnung::Rechnung(const std::string& nameInit, const std::string& addressInit, std::shared_ptr<AbstrakterVertrag> dealInit) :
+    name(nameInit), address(addressInit), deal(dealInit) {}
+std::shared_ptr<AbstrakterVertrag> Rechnung::getVertrag() {
+    return deal;
+}
+// Rechnungsbetrag: XXX.XX€
+std::ostream& operator<< (std::ostream& s, const Rechnung& check) {
+    return s << "Rechnungsbetrag: " << std::fixed << std::setprecision(2) << check.deal -> berechneKosten() << "€";
+}
---
+++ B07/vertraege.hh
@@ -0,0 +1,93 @@
+#ifndef H_VERTRAEGE
+#define H_VERTRAEGE
+
+#include <cmath>
+#include <vector>
+#include <memory>
+
+// a)
+class AbstrakterVertrag {
+    public:
+        virtual double berechneKosten() const = 0;
+        virtual void verbraucheTelefonEinheiten(double) = 0;
+        virtual void verbraucheSMSEinheit() = 0;
+};
+
+// b)
+class FlatrateVertrag : public AbstrakterVertrag {
+    protected:
+        double fixrate;
+    public:
+        FlatrateVertrag(double fixrateInit);
+        
+        virtual double berechneKosten() const;
+        virtual void verbraucheTelefonEinheiten(double);
+        virtual void verbraucheSMSEinheit();
+};
+
+// c)
+class GrundgebuehrVertrag : public AbstrakterVertrag {
+    protected:
+        double fixedCharge;
+        double callMinutes;
+        int smsNum;
+        double callPrise, smsPrise;
+    public:
+        GrundgebuehrVertrag(double fixedChargeInit, double callPriseInit, double smsPriseInit);
+        
+        virtual double berechneKosten() const;
+        virtual void verbraucheTelefonEinheiten(double min);
+        virtual void verbraucheSMSEinheit();
+};
+class OhneGrundgebuehrVertrag : public GrundgebuehrVertrag {
+    public:
+        OhneGrundgebuehrVertrag(double callPriseInit, double smsPriseInit);
+};
+
+// d)
+class SekuendlicherVertrag: public GrundgebuehrVertrag {
+    public:
+        SekuendlicherVertrag(double fixedChargeInit, double callPriseInit, double smsPriseInit);
+        virtual void verbraucheTelefonEinheiten(double min);
+};
+
+// e)
+class GuthabenVertrag : public OhneGrundgebuehrVertrag {
+    protected:
+        //double balance = 0;
+        double fixedCredit;
+        //double check = 0;
+    public:
+        GuthabenVertrag(double callPriseInit, double smsPriseInit, double fixedCreditInit);
+        virtual double berechneKosten() const;
+};
+
+// f)
+class VertragsBundle : public AbstrakterVertrag {
+    std::vector<std::shared_ptr<AbstrakterVertrag>> deals;
+    std::shared_ptr<AbstrakterVertrag> mainDeal;
+    public:
+        VertragsBundle(std::shared_ptr<AbstrakterVertrag> mainDealInit);
+
+        virtual double berechneKosten() const;
+        virtual void verbraucheTelefonEinheiten(double);
+        virtual void verbraucheSMSEinheit();
+
+        std::shared_ptr<AbstrakterVertrag> get();
+        std::shared_ptr<AbstrakterVertrag> get(unsigned int i);
+        void vertragRegistrieren(std::shared_ptr<AbstrakterVertrag> subDeal);
+};
+
+// g)
+class Rechnung {
+    private:
+        std::string name;
+        std::string address;
+        std::shared_ptr<AbstrakterVertrag> deal;
+    public:
+        Rechnung(const std::string& nameInit, const std::string& addressInit, std::shared_ptr<AbstrakterVertrag> dealInit);
+        std::shared_ptr<AbstrakterVertrag> getVertrag();
+        friend std::ostream& operator<< (std::ostream& s, const Rechnung& check);
+};
+
+#endif
